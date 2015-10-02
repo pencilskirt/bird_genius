3da07e245c4f4d367e78eac4b883c848f20015bb
Lea Ingold <pencilskirt256@gmail.com>
2015-10-02 19:05:17 -0400

    first commit

--- BirdGenius3/ViewController.h
+++ BirdGenius3/ViewController.h
@@ -2,13 +2,23 @@
 //  ViewController.h
 //  BirdGenius3
 //
-//  Created by The Ingolds on 10/2/15.
+//  Created by Lea Ingolds on 10/2/15.
 //  Copyright © 2015 Pencilskirt Studio. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+
+@property (strong, nonatomic) IBOutlet UIImageView *birdImageView;
+
+@property (nonatomic) NSInteger randomIndex;
+
+@property (nonatomic) NSDictionary *birdDict;
+
+
 
 
 @end
--- BirdGenius3/ViewController.m
+++ BirdGenius3/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import "NSMutableArray+Shuffle.h"
 @interface ViewController ()
+
 
 @end
 
@@ -17,11 +18,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    
 }
 
+
+
+
+- (IBAction)swipeCardLeft:(id)sender {
+    NSDictionary *birdDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                              @"1.png",@"robin",
+                              @"2.png",@"chaffinch",
+                              @"3.png",@"doves",
+                              @"4.png",@"kestrel",
+                              nil];
+    
+    _randomIndex = arc4random()% [birdDict count];
+    
+    NSArray *myKeys = [birdDict allKeys];
+
+    
+    NSInteger index = (NSInteger)[self randomIndex];
+    
+    NSString *thisKey = [myKeys objectAtIndex: index];
+    NSString *thisObject = [birdDict objectForKey: thisKey];
+    
+    _nameLabel.text = thisKey;
+    _birdImageView.image = [UIImage imageNamed:thisObject];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
 @end
+
